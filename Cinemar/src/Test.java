5c75afa5c194d625aafca0cd219be5d919dc0357
marcesalas96 <99264862+marcesalas96@users.noreply.github.com>
2022-07-07 23:43:08 -0300

    Subida carpeta Cinemar
    Aqui esta todo mi avance, mañana voy a ponerme a full con esto

---
+++ Cinemar/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		Cliente juan  = new Cliente();
+		juan.registrarCliente();
+		juan.aumentarHistorial();
+		juan.aumentarHistorial();
+		juan.aumentarHistorial();
+		juan.aumentarHistorial();
+		
+	}
+
+}
---
+++ Cinemar/src/Usuario.java
@@ -0,0 +1,39 @@
+public class Usuario {
+	protected String usuario;
+	protected String nombre;
+	protected String apellido;
+	protected int edad;
+	protected String correo;
+	protected String contraseña;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+}
